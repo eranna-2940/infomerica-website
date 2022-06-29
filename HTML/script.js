b04aa36de8f39efe153ef22a30444ed95c2307e5
GUTTI ERANNA <81354309+eranna-2940@users.noreply.github.com>
2022-06-29 11:00:21 +0530

    Add files via upload

---
+++ HTML/script.js
@@ -0,0 +1,43 @@
+
+function validemail() {
+        let email = document.getElementById("email").value
+        let erroremail = document.getElementById("erroremail")
+
+        if (email.endsWith('gmail')) {
+            erroremail.innerHTML = ""
+        } else {
+            erroremail.innerHTML = "please enter gmail.com";
+            erroremail.style.color = 'red';
+        }
+    }
+
+    function validpassword() {
+        let password = document.getElementById("password").value
+        let errorpassword = document.getElementById("errorpassword").value
+
+        if (password.length < 3) {
+            errorpassword.innerHTML = "please enter strong password"
+            errorpassword.style.color = 'red';
+        } else {
+
+            errorpassword.innerHTML = ""
+        }
+    }
+
+
+    function getFormvalue() {
+        let r_email = document.getElementById("email");
+        let r_password = document.getElementById("password");
+
+        localStorage.setItem('email', r_email.value);
+        localStorage.setItem('password',r_password.value);
+
+        document.write(`
+            {Email:${r_email.value}<br>
+            Password:${r_password.value}}
+
+        `);
+
+    };
+
+
